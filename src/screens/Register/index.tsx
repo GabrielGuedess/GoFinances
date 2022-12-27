fb493f59f1b20bd488ce62be59cf9a875b10f6f9
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-12-27 15:19:28 -0300

    fix: yup price validation

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -28,7 +28,8 @@ const schema = Yup.object().shape({
   name: Yup.string().required('Nome é obrigatório'),
   amount: Yup.number()
     .typeError('Informe um valor numérico')
-    .positive('O valor não pode ser negativo'),
+    .positive('O valor não pode ser negativo')
+    .required('Preço é obrigatório'),
 });
 
 export function Register({ ...props }: ViewProps) {
